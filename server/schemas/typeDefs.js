3ca09d67f2bf749c393423a181cf68ea20a64482
alexslzr <alejandroslzr17@gmail.com>
2023-08-09 18:24:49 -0600

    setup server.js for working with apollo

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,37 @@
+const {gql} = require('apollo-server-express')
+
+const typeDefs = gql`
+type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+}
+
+type Book {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image:
+    link:
+}
+
+type Auth {
+    token: Int
+    user: User
+}
+
+type Query {
+    me: User
+}
+
+type Mutation {
+    login
+    addUser
+    saveBook
+    removeBook
+}`
+
+module.exports =  typeDefs
